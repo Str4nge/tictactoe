cf91c75112df095df8ae026baea5fd8beb7f57f6
Str4nge <code.amines@gmail.com>
2021-06-21 20:30:05 +0530

    added winner algo

--- components/Board.js
+++ components/Board.js
@@ -1,35 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
-const Board = () => {
-  const [board, setBoard] = useState(Array(9).fill(null)); // useState hook will take the initial state and return an array of exactly 2 elements
-  const [isXnext, setIsXnext] = useState(false); //  These elements destructured here are the instance of current state and the function to update the state
+const Board = ({ board, handleSquareClick }) => {
+  // const [board, setBoard] = useState(Array(9).fill(null)); // useState hook will take the initial state and return an array of exactly 2 elements
+  // const [isXnext, setIsXnext] = useState(false); //  These elements destructured here are the instance of current state and the function to update the state
 
-  // setBoard is used to update the state by passing the updating value OR by passing a function that will update the value.
-  // setBoard('X') will simply update the value of board with 'X'
+  // // setBoard is used to update the state by passing the updating value OR by passing a function that will update the value.
+  // // setBoard('X') will simply update the value of board with 'X'
 
-  // handle click on the square
-  const handleSquareClick = pos => {
-    // if the board[pos] is not null i.e. one mark on the square already exists then just return
-    if (board[pos]) {
-      return;
-    }
+  // // handle click on the square
+  // const handleSquareClick = pos => {
+  //   // if the board[pos] is not null i.e. one mark on the square already exists then just return
+  //   if (board[pos]) {
+  //     return;
+  //   }
 
-    setBoard(prev => {
-      return prev.map((square, position) => {
-        // if the position on which map is mapping equals the clicked position then put a mark, else return the same value
-        if (pos === position) {
-          return isXnext ? 'X' : 'O'; // if the isNext state true then put 'X', else put 'O'
-        }
-        return square;
-      });
-    });
+  //   setBoard(prev => {
+  //     return prev.map((square, position) => {
+  //       // if the position on which map is mapping equals the clicked position then put a mark, else return the same value
+  //       if (pos === position) {
+  //         return isXnext ? 'X' : 'O'; // if the isNext state true then put 'X', else put 'O'
+  //       }
+  //       return square;
+  //     });
+  //   });
 
-    // toggle the next state after click handling has happened to change 'X' to 'O'
-    setIsXnext(prev => {
-      return !prev;
-    });
-  };
+  //   // toggle the next state after click handling has happened to change 'X' to 'O'
+  //   setIsXnext(prev => {
+  //     return !prev;
+  //   });
+  // };
 
   // render and return a Square component with value and an onClick event ( that is handled by the square component using props)
   // it will generate a square and whenever the square is clicked the handleSquare function is called using the position of the square
--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from '../components/Board';
+import { calculateWinner } from './winner';
 import './styles/root.scss';
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null)); // useState hook will take the initial state and return an array of exactly 2 elements
+  const [isXnext, setIsXnext] = useState(false); //  These elements destructured here are the instance of current state and the function to update the state
+
+  const winner = calculateWinner(board);
+
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next is ${isXnext ? 'X' : 'O'}`;
+  // setBoard is used to update the state by passing the updating value OR by passing a function that will update the value.
+  // setBoard('X') will simply update the value of board with 'X'
+
+  // handle click on the square
+  const handleSquareClick = pos => {
+    // if the board[pos] is not null i.e. one mark on the square already exists or WINNER exists then just return
+    if (board[pos] || winner) {
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((square, position) => {
+        // if the position on which map is mapping equals the clicked position then put a mark, else return the same value
+        if (pos === position) {
+          return isXnext ? 'X' : 'O'; // if the isNext state true then put 'X', else put 'O'
+        }
+        return square;
+      });
+    });
+
+    // toggle the next state after click handling has happened to change 'X' to 'O'
+    setIsXnext(prev => {
+      return !prev;
+    });
+  };
+
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <h3>message</h3>
-      <Board />
+      <h3>{message}</h3>
+      <Board board={board} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
